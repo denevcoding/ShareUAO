259c48ecf37cd5307c3d4dfcd4c0f16c84a2a480
DgiraldoB34 <danielgiraldobueno@gmail.com>
2022-09-10 07:52:23 -0500

    Montaje de Escena inicial
    falta acomodar todo pero ya estan algunos assets en el programa

---
+++ Assets/Scripts/movimientoanimacion.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoanimacion : MonoBehaviour
+{
+    private Animator gatoimoportado;
+    private void Awake()
+    {
+        gatoimportado = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Imput.GetkeyDown (KeyCode.W))
+        {
+            gatoimportado.SetBool("moving", true);
+        }
+       if(Input.GetKeyUp(KeyCode.W))
+        {
+            gatoimportado.SetBool("moving", false);
+        }
+    }
+}
