bc5b02301cc47bd566af80ef422d3262325b3782
DgiraldoB34 <danielgiraldobueno@gmail.com>
2022-09-19 17:42:41 -0500

    nueva escena

--- Assets/Scripts/camara.cs
+++ Assets/Scripts/camara.cs
@@ -13,6 +13,6 @@ public class camara : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Input.GetAxis("Mouse Y"), 0, 0);
+        transform.Rotate(Input.GetAxis("Mouse Y")*-1, 0, 0);
     }
 }
