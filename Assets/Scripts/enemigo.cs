ed086c5021c08e3449e0267e2e9597044cd8d696
DgiraldoB34 <danielgiraldobueno@gmail.com>
2022-09-20 10:10:46 -0500

    dos enemigos con la inteligencia mejorada.

--- Assets/Scripts/enemigo.cs
+++ Assets/Scripts/enemigo.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class enemigo : MonoBehaviour
 {
     public int vida = 3;
-    public int comportamiento = 1; // 1 va a ser para perseguir y el 2 para patrullar
+   // public int comportamiento = 1; // 1 va a ser para perseguir y el 2 para patrullar
     GameObject jugador;
     // Start is called before the first frame update
     void Start()
     {
         jugador = GameObject.Find("Atun _Character");
-        comportamiento = Random.Range(1, 3);
+        //comportamiento = Random.Range(1, 3);
 
         InvokeRepeating("CambiarDireccion", 3, 3);
     }
@@ -19,11 +20,11 @@ public class enemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(comportamiento == 1)
-        {
+      //  if(comportamiento == 1)
+       // {
             transform.LookAt(jugador.transform);
             GetComponent<Rigidbody>().velocity = transform.forward * 2;
-        }
+      //  }
         //else
         //{
           //  GetComponent<Rigidbody>().velocity = transform.forward ;
@@ -39,9 +40,13 @@ public class enemigo : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.name == "Atun _Character") 
+        if (collision.collider.name == "Atun _Character")
+        {
+            Debug.Log("Miau, nos acaban de agarrar");
+            SceneManager.LoadScene(0);
 
-        Debug.Log("Miau, nos acaban de agarrar");
+        }
+        
 
         if (collision.collider.tag == "Bala")
         {
--- Assets/Scripts/villano.cs
+++ Assets/Scripts/villano.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class villano : MonoBehaviour
 {
     public GameObject item;
+    public int velocidad;
+    public int vida;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,26 @@ public class villano : MonoBehaviour
     void Update()
     {
         transform.LookAt(item.transform);
-        GetComponent<Rigidbody>().velocity = transform.right * 5;
+        GetComponent<Rigidbody>().velocity = transform.right * velocidad;
     }
-}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Atun _Character")
+        {
+            Debug.Log("Miau, nos acaban de agarrar");
+            SceneManager.LoadScene(0);
+
+        }
+
+
+        if (collision.collider.tag == "Bala")
+        {
+            Destroy(collision.collider.gameObject);
+            vida = vida - 1;
+            if (vida == 0)
+                Destroy(gameObject);
+        }
+    }
+
+    }
