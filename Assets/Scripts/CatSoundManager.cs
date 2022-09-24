a0048b8193e70281510120c893796ae6c22e2c88
xUsuari0 <starrk037@gmail.com>
2022-09-23 21:18:40 -0500

    Sound Manager created

---
+++ Assets/Scripts/CatSoundManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSoundManager : MonoBehaviour
+{
+    public AudioSource catSource;
+    public AudioSource vfxSource;
+
+    [Space(20)]
+    public AudioClip step;
+    public AudioClip stepChunco;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound(AudioClip clip) 
+    {
+        if (catSource.clip != clip) 
+        {
+            catSource.clip = clip;
+            catSource.Play();
+        }
+       
+    }
+
+    public void PlayOneShot(AudioClip clip, float volume)
+    {
+        catSource.PlayOneShot(clip, volume);
+    }
+
+
+
+    public void CleanClip()
+    {
+        if (catSource != null)
+            catSource.clip = null;
+    }
+
+
+
+
+    public void PlayStep(int stepIndex)
+    {
+        if (stepIndex ==0)
+        {
+            catSource.PlayOneShot(step, 1);
+        }
+        else if (stepIndex == 1)
+        {
+            catSource.PlayOneShot(stepChunco, 1);
+        }
+
+
+       
+    }
+}
